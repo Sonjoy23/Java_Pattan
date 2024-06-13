849293f223f542f87959163b8a816a20ca26c79d
Sonjoy Das <141509534+Sonjoy23@users.noreply.github.com>
2024-06-13 17:13:26 +0600

    Add files via upload

---
+++ pattan5.java
@@ -0,0 +1,17 @@
+public class pattan5 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            for(int j=i; j<=10; j++){
+                System.out.print("*");
+            }
+            for(int j=0+i; j<10; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
